caca044b1c484c1d18f995b180554b99446b9ed4
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-03-16 17:35:31 -0300

    feat: add senders attribute in order to log the addresses that waved

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    address[] senders;
 
     constructor() {
         console.log("Yo, I am a contract and I am smart");
@@ -13,11 +14,18 @@ contract WavePortal {
 
     function wave() public {
         totalWaves += 1;
+        senders.push(msg.sender);
+
         console.log("%s has waved", msg.sender);
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves!", totalWaves);
+        console.log("\nWe have %d total waves!", totalWaves);
+
+        console.log("The senders was: \n");
+        for (uint256 i = 0; i < senders.length; i++) {
+            console.logAddress(senders[i]);
+        }
         return totalWaves;
     }
 }
